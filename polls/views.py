c1954f833345517db92b70a237ab39900828b0ae
frankliang2 <348017715@qq.com>
2015-10-14 16:24:48 +0800

    Tutorial 4 Form View

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Question,Choice
 from django.template import RequestContext,loader
 from django.http import Http404
 
@@ -14,8 +15,16 @@ def detail(request,question_id):
 	return render(request,'polls/detail.html',{'question' : question})
 
 def results(request,question_id):
-	response = "You are looking at the results of %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question,pk=question_id)
+	return render(request,'polls/results.html',{'question' : question})
 
 def vote(request,question_id):
-	return HttpResponse("You are voting at %s" % question_id)
+	p = get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except(KeyError,Choice.DoesNotExist):
+		return render(request,'polls/detail.html',{'question' : p,'error_message' : "You didn't select a choice"})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
