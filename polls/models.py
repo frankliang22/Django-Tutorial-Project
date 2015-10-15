c8eeafda80a096b6656b40aca00aa3a1f3255770
frank liang <frank@frankdeMacBook-Pro.local>
2015-10-15 23:21:45 +0800

    finish the test part

--- polls/models.py
+++ polls/models.py
@@ -6,11 +6,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now().date() - datetime.timedelta(days=1)
-    	was_published_recentlu.admin_order_field = 'pub_date'
-    	was_published_recentlu.boolean = True
-    	was_published_recentlu.short_description = 'Published recently?'
-
+        yesterday = timezone.now().date() - datetime.timedelta(days=1)
+    	return self.pub_date <= yesterday
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+import datetime
+from .models import Question
+from django.utils import timezone
 
-# Create your tests here.
+class QuestionMethodTests(TestCase):
+	
+	def test_was_published_recently_with_future_question(self):
+		time = timezone.now().date() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently,False)
+	
+	
